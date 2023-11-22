f5c2d94276e3e25870cc746cbaf5e932e64db470
Gabriel Rosa <gabriel.rosa@dcc.ufmg.br>
2023-11-22 07:10:49 -0300

    try fix models regression

--- src/executeAll.py
+++ src/executeAll.py
@@ -76,31 +76,31 @@ def trainModels(dataName, setDivision):
     try:
         # ------------------------ Recupera os modelos já otimizados --------------------------
         print(f"    | Etapa 3 {dataName} - Obtendo modelos já otimizados!")
-        SVM, KNN, LR = GetModelsClassification(dataName)        # Obtém os modelos de classificação
+        # SVM, KNN, LR = GetModelsClassification(dataName)        # Obtém os modelos de classificação
         LSTM, MLP, RNN = GetModelsRegression(dataName)          # Obtém os modelos de regressão
 
         # --------------------------- Treina os modelos ---------------------------
         print(f"    | Etapa 4 {dataName} - Treinando modelos!")
         train_time = time.time()
-        Y_Train_statistic = pd.read_csv(f'../Data/Cut/statistic/Y/Train_{setDivision[1]}{dataName}.csv', sep=";")['OutPut |T+1|']   # Obtém os dados de treino para os modelos de estatística
-        Y_Test_statistic  = pd.read_csv(f'../Data/Cut/statistic/Y/Test_{dataName}.csv', sep=";")['OutPut |T+1|']    # Obtém os dados de teste para os modelos de estatística
-        X_Test_dataset1  = pd.read_csv(f'../Data/Cut/dataset1/X/Test_{dataName}.csv', sep=";")                      # Obtém os dados de teste para os modelos de classificação
+        # Y_Train_statistic = pd.read_csv(f'../Data/Cut/statistic/Y/Train_{setDivision[1]}{dataName}.csv', sep=";")['OutPut |T+1|']   # Obtém os dados de treino para os modelos de estatística
+        # Y_Test_statistic  = pd.read_csv(f'../Data/Cut/statistic/Y/Test_{dataName}.csv', sep=";")['OutPut |T+1|']    # Obtém os dados de teste para os modelos de estatística
+        # X_Test_dataset1  = pd.read_csv(f'../Data/Cut/dataset1/X/Test_{dataName}.csv', sep=";")                      # Obtém os dados de teste para os modelos de classificação
         X_Train_dataset2 = pd.read_csv(f'../Data/Cut/dataset2/X/Train_{setDivision[1]}{dataName}.csv', sep=";")                     # Obtém os dados de treino para os modelos de regressão
         Y_Train_dataset2 = pd.read_csv(f'../Data/Cut/dataset2/Y/Train_{setDivision[1]}{dataName}.csv', sep=";")['OutPut |T+1|']     # Obtém os dados de treino para os modelos de regressão
         X_Test_dataset2  = pd.read_csv(f'../Data/Cut/dataset2/X/Test_{dataName}.csv', sep=";")                      # Obtém os dados de teste para os modelos de regressão
         Y_Test_dataset2  = pd.read_csv(f'../Data/Cut/dataset2/Y/Test_{dataName}.csv', sep=";")['OutPut |T+1|']      # Obtém os dados de teste para os modelos de regressão
 
-        ClassificationModels = [SVM, KNN, LR]           # Lista de modelos de classificação
-        ClassificationNames  = ['SVM', 'KNN', 'LR']     # Lista de nomes dos modelos de classificação
+        # ClassificationModels = [SVM, KNN, LR]           # Lista de modelos de classificação
+        # ClassificationNames  = ['SVM', 'KNN', 'LR']     # Lista de nomes dos modelos de classificação
         RegressionModels = [LSTM, MLP, RNN]             # Lista de modelos de regressão
         RegressionNames  = ['LSTM', 'MLP', 'RNN']       # Lista de nomes dos modelos de regressão
 
-        print(f"        - Etapa 4.1 {dataName} - Treinando modelos de classificação")
-        GetClassificationPredictions(dataName, ClassificationModels, ClassificationNames, X_Test_dataset1)                                              # Obtém as predições dos modelos de classificação
+        # print(f"        - Etapa 4.1 {dataName} - Treinando modelos de classificação")
+        # GetClassificationPredictions(dataName, ClassificationModels, ClassificationNames, X_Test_dataset1)                                              # Obtém as predições dos modelos de classificação
         print(f"        - Etapa 4.2 {dataName} - Treinando modelos de regressão")
         GetRegressionPredictions(dataName, RegressionNames, RegressionModels, X_Test_dataset2, Y_Test_dataset2, X_Train_dataset2, Y_Train_dataset2)     # Obtém as predições dos modelos de regressão
-        print(f"        - Etapa 4.3 {dataName} - Treinando modelos de estatística")
-        GetStatisticPredictions(dataName, Y_Train_statistic.ravel(), Y_Test_statistic.ravel(), window=100)                                              # Obtém as predições dos modelos de estatística
+        # print(f"        - Etapa 4.3 {dataName} - Treinando modelos de estatística")
+        # GetStatisticPredictions(dataName, Y_Train_statistic.ravel(), Y_Test_statistic.ravel(), window=100)                                              # Obtém as predições dos modelos de estatística
         train_time = time.time() - train_time
         print(f"    | Etapa 4 {dataName} - Time: {train_time}")
     except Exception as e:
